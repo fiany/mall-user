d0bf7fa736c69dcccbbc4051da1d75ef52c359db
fiany <f848021126@outlook.com>
2020-11-10 23:36:39 +0800

    feat: 框架初始化

---
+++ src/pages/shopping-cart/shopping-cart.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="content">
+		<button type="primary">hello</button>
+		<uni-calendar 
+		    :insert="true"
+		    :lunar="true" 
+		    :start-date="'2019-3-2'"
+		    :end-date="'2019-5-20'"
+		    @change="change"
+		     />
+	</view>
+</template>
+
+<script>
+	import uniCalendar from '@/components/uni-calendar/uni-calendar.vue'
+	export default {
+		components: {
+			uniCalendar
+		}
+	}
+</script>
+
+<style>
+</style>
